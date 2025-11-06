7424787a06f3c661823412b03bbb33267d81ece2
ashraful-04 <sohelalom444@gmail.com>
2025-11-06 20:34:46 +0530

    Updated Home.jsx to show full short URL

--- Pitly-The-URL-Shortener-main/client/src/main.jsx
+++ Pitly-The-URL-Shortener-main/client/src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
         domain={import.meta.env.VITE_AUTH0_DOMAIN}
         clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
         authorizationParams={{
-          redirect_uri: "http://localhost:5173/app"
+          redirect_uri: "https://pitly-frontend.onrender.com/app"
         }}
       >
         <Routes>
