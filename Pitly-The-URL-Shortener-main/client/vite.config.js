a3bb954cf6d47cd0fb0060c170546c42a080dfeb
ashraful-04 <sohelalom444@gmail.com>
2025-11-06 23:08:20 +0530

    Fix vite config and redirect issue

--- Pitly-The-URL-Shortener-main/client/vite.config.js
+++ Pitly-The-URL-Shortener-main/client/vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // ensures Vite outputs build files here
+    outDir: 'dist', // build output folder
+    emptyOutDir: true, // ensures clean build
   },
-  publicDir: 'public', // ensures _redirects file is copied during build
 })
-
